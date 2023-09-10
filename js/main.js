6ef111cc5491310789c8b766b467fcb055bc5a48
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-10 10:08:16 -0600

    modal window

--- js/main.js
+++ js/main.js
@@ -1,11 +1,13 @@
 const modal = document.querySelector(".modal");
-const button1 = document.querySelector("#button1");
+const btn_navbar = document.querySelectorAll(".btn-navbar");
 const closeButton = document.querySelector(".modal__content__close");
 
-button1.addEventListener("click", () => {
-  modal.classList.remove("hidden");
-  modal.classList.add("visible");
-});
+for (let i = 0; i < btn_navbar.length; i++) {
+  btn_navbar[i].addEventListener("click", () => {
+    modal.classList.remove("hidden");
+    modal.classList.add("visible");
+  });
+}
 
 closeButton.addEventListener("click", () => {
   modal.classList.add("hidden");
