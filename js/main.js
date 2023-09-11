ded1ad133ac96ebb81e441d7e8a8ed4ba20ff128
AdrianGerman <adrian.german4485@alumnos.udg.mx>
2023-09-10 20:36:13 -0600

    CSS layout arrangement

--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector(".modal");
 const btn_navbar = document.querySelectorAll(".btn-navbar");
 const closeButton = document.querySelector(".modal__content__close");
+const homeButton = document.querySelector(".btn-modal");
 
 for (let i = 0; i < btn_navbar.length; i++) {
   btn_navbar[i].addEventListener("click", () => {
@@ -12,4 +13,8 @@ for (let i = 0; i < btn_navbar.length; i++) {
 closeButton.addEventListener("click", () => {
   modal.classList.add("hidden");
   modal.classList.remove("visible");
+});
+
+homeButton.addEventListener("click", () => {
+  window.location.href = "index.html";
 });
